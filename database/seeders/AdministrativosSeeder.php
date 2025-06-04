91d7d10e35902594ef9c49fa711914d6ed6ba811
Jared Gonzalez <al222310412@gmail.com>
2025-06-04 14:38:55 -0600

    Controladores, modelos y migraciones creadas

---
+++ database/seeders/AdministrativosSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AdministrativosSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
