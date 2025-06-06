ed1f7362ff2a7813ef8f08ab352e93b8ee4c6d0d
Jared Gonzalez <al222310412@gmail.com>
2025-06-06 09:59:32 -0600

    Middlewares creados y modificados para delimitar el acceso por rol

---
+++ app/Http/Middleware/AdminIsAuthenticated.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class AdminIsAuthenticated
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if(!Auth::check()) {
+            return redirect()->route('login')->with('error', 'Debes iniciar sesión para acceder a esta página.');
+        }
+        if(Auth::user()->role !== 'Administrativo') {
+            if(Auth::user()->role === 'Maestro') {
+                return redirect()->route('maestro.home')->with('error', 'No tienes permisos para acceder a esta página.');
+            } elseif(Auth::user()->role === 'Alumno') {
+                return redirect()->route('alumno.home')->with('error', 'No tienes permisos para acceder a esta página.');
+            }
+            // Si el usuario no es Administrativo, Maestro o Alumno, redirigir a la página de inicio del Administrativo
+            return redirect()->route('admin.home')->with('error', 'No tienes permisos para acceder a esta página.');
+        }
+        return $next($request);
+    }
+}
