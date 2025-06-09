5988be876f423ba534fdc9baa030bed7a3a4f86b
Jared Gonzalez <al222310412@gmail.com>
2025-06-09 14:57:40 -0600

    Modificación de seeder para creación de semestres

--- database/seeders/SemestreSeeder.php
+++ database/seeders/SemestreSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class SemestreSeeder extends Seeder
 {
@@ -12,6 +13,21 @@ class SemestreSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $semestres = [
+            ['Semestre' => 'Primero', 'Status' => 1],
+            ['Semestre' => 'Segundo', 'Status' => 1],
+            ['Semestre' => 'Tercero', 'Status' => 1],
+            ['Semestre' => 'Cuarto', 'Status' => 1],
+            ['Semestre' => 'Quinto', 'Status' => 1],
+            ['Semestre' => 'Sexto', 'Status' => 1],
+            ['Semestre' => 'Séptimo', 'Status' => 1],
+            ['Semestre' => 'Octavo', 'Status' => 1],
+            ['Semestre' => 'Noveno', 'Status' => 1],
+            ['Semestre' => 'Décimo', 'Status' => 1],
+            ['Semestre' => 'Undécimo', 'Status' => 1],
+            ['Semestre' => 'Duodécimo', 'Status' => 1]
+        ];
+
+        DB::table('semestres')->insert($semestres);
     }
 }
